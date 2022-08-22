23239bfb66689442f6358993de63c7f587e9c0ad
matteobianchi <matteo.bianchi@hec.edu>
2022-08-22 17:30:40 +0200

    Edit, Update and Destroy done, CSS TBC

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -20,6 +20,26 @@ class BikesController < ApplicationController
     end
   end
 
+  def edit
+    @bike = Bike.find(params[:id])
+  end
+
+  def update
+    @bike = Bike.find(params[:id])
+    @bike.update(bike_params)
+
+    redirect_to bike_path(@bike)
+  end
+
+  def destroy
+    @bike = Bike.find(params[:id])
+    @bike.destroy
+
+    redirect_to bikes_path, status: :see_other
+  end
+
+  private
+
   def bike_params
     params.require(:bike).permit(:type, :brand, :description, :address, :price_per_day)
   end
