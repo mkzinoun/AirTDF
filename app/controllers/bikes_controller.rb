e413353e1302d73b85ef40f41a89ce6b0627dd5f
Ariel Jurkevitch <arieljurkevitch@gmail.com>
2022-08-23 12:20:10 +0200

    Bookings controller create on bike/show

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -7,6 +7,7 @@ class BikesController < ApplicationController
 
   def show
     @bike = Bike.find(params[:id])
+    @booking = Booking.new
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :bikes
+  resources :bikes do
+    resources :bookings, only: [:create]
+  end
 end
